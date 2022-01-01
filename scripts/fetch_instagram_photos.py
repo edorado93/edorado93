f20900e05fef1cdb1908a1c52d0cfdc7e8157a09
Sachin Malhotra <sacmalho@microsoft.com>
2022-01-01 01:22:32 -0800

    Added instagram action

---
+++ scripts/fetch_instagram_photos.py
@@ -0,0 +1,28 @@
+import instaloader
+
+def fetch_recent_K_photos(K, handle, loader_instance):
+    profile = instaloader.Profile.from_username(L.context, handle)
+    posts = profile.get_posts()
+    top_K_posts = []
+    for p in posts:
+        top_K_posts.append(p)
+        K -= 1
+
+        if not K:
+            return top_K_posts
+
+    return top_K_posts
+
+if __def__ == "__main__":
+
+    L = instaloader.Instaloader(dirname_pattern="instagram_posts\\{target}", filename_pattern="post")
+    handle_to_consider = os.getenv("INSTAGRAM_PUBLIC_HANDLE")
+    recent_posts = fetch_recent_K_photos(3, handle_to_consider, L)
+
+    index = 0
+    for post in recent_posts:
+        L.download_post(post, target=("post_" + str(index)))
+        index += 1
+
+
+        
