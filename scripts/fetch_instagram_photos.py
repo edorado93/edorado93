1283fd516b1a306f0d7012ed2723800eec4e8148
Sachin Malhotra <sachinmalhotra1993@gmail.com>
2022-01-11 09:20:18 -0800

    Update fetch_instagram_photos.py

--- scripts/fetch_instagram_photos.py
+++ scripts/fetch_instagram_photos.py
@@ -1,5 +1,5 @@
 import instaloader
-import os
+import os, glob
 
 def fetch_recent_K_photos(K, handle, loader_instance):
     profile = instaloader.Profile.from_username(L.context, handle)
@@ -14,12 +14,20 @@ def fetch_recent_K_photos(K, handle, loader_instance):
 
     return top_K_posts
 
+def clean_old_photos(path, num_photos):
+    for i in range(num_photos):
+        files = glob.glob(path + "/post" + str(i))
+        for f in files:
+            os.remove(f)
+
 if __name__ == "__main__":
 
     L = instaloader.Instaloader(dirname_pattern="instagram_posts/{target}", filename_pattern="post")
     handle_to_consider = os.getenv("INSTAGRAM_PUBLIC_HANDLE")
     recent_posts = fetch_recent_K_photos(3, handle_to_consider, L)
 
+    clean_old_photos("instagram_posts", 3)
+    
     index = 0
     for post in recent_posts:
         L.download_post(post, target=("post_" + str(index)))
